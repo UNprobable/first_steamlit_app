69841d201290215b0f9af738618bd22e2d446efd
UNprobable <105222386+UNprobable@users.noreply.github.com>
2022-05-13 16:17:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response.json())
 
 #take the json version of the response and normalize it
-fruittyvice_normalizes = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it the screen as a table
-streamlit.dataframe(fruitvyvice_normalized)
+streamlit.dataframe(fruityvice_normalized)
