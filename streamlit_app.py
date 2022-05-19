c336a2f35d1f287a74943f0fae99bff0cda5371e
UNprobable <105222386+UNprobable@users.noreply.github.com>
2022-05-19 17:39:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,4 @@ streamlit.dataframe(my_data_rows)
 
 
 add_my_fruit = streamlit.text_input('What fruit would you like information about? ', 'jackfruit')
-#streamlit.write('thanks for adding',add_my_fruit)
+streamlit.write('thanks for adding',add_my_fruit)
